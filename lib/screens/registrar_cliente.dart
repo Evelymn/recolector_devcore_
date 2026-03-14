88ff0749623fdc284051524929043f4c02168345
Evelymn <evelyncastroel711360@gmail.com>
2026-03-13 23:07:06 -0600

    DEV-16: Estructura inicial pantalla registral cliente

---
+++ lib/screens/registrar_cliente.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class RegistrarCliente extends StatefulWidget {
+  const RegistrarCliente({super.key});
+
+  @override
+  State<RegistrarCliente> createState() => _RegistrarClienteState();
+}
+
+class _RegistrarClienteState extends State<RegistrarCliente> {
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController direccionController = TextEditingController();
+  final TextEditingController coloniaController = TextEditingController();
+  final TextEditingController telefonoController = TextEditingController();
+  final TextEditingController estadoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Registrar Cliente")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            TextField(
+              controller: nombreController,
+              decoration: const InputDecoration(labelText: "Nombre"),
+            ),
+
+            TextField(
+              controller: direccionController,
+              decoration: const InputDecoration(labelText: "Dirección"),
+            ),
+
+            TextField(
+              controller: coloniaController,
+              decoration: const InputDecoration(labelText: "Colonia"),
+            ),
+
+            TextField(
+              controller: telefonoController,
+              decoration: const InputDecoration(labelText: "Teléfono"),
+            ),
+
+            TextField(
+              controller: estadoController,
+              decoration: const InputDecoration(labelText: "Estado"),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                print("Guardar cliente");
+              },
+              child: const Text("Guardar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
