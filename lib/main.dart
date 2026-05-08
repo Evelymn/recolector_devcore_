377d526b52799ce16e734bbe36eae3b47b90be3e
Juan <naujralios01@gmail.com>
2026-05-07 22:31:11 -0600

    cabiar el nombre de archivo registrar_clientes a dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'screens/usuarios_page.dart';
-import 'screens/registrar_cliente.dart';
+import 'screens/dashboard.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
